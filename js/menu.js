6d960e9bdc7c0bc64483523aa1d1f527151fc924
chaki1997 <davit.chaki.97@gmail.com>
2018-08-31 11:26:10 -0700

    football

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+	$(document).ready(function(){
+
+	$('.toggle').click(function(){
+		$('.header_footer').toggleClass('active');
+		$('header').toggleClass('actived');
+		$('.invest').toggleClass('activeded');
+		$('.logo').toggleClass('activededd');
+	});
+});
